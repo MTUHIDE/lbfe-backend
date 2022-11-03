0ae4abf0b7bd3c62d1ee9290a6335b1274cc7caa
Justin <jmilliman@serviceexpress.com/>
2022-11-03 16:58:00 -0400

    [bugfix[ Nasty config workaround
    This is a temporary solution

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,17 @@
 // Load environment variables into a struct so we can actually use them
 // these will be set via the local .env file
 module.exports = {
-    app: process.env.APP,
-    port: parseInt(process.env.PORT, 10),
-    session_secret: process.env.SESSION_SECRET,
+    app: process.env.APP || 'dev',
+    port: parseInt(process.env.PORT, 10) || '9010',
+    session_secret: process.env.SESSION_SECRET || 'Megaworm',
     log_level: process.env.LOG_LEVEL || 'info',
 
     // DB
-    db_host: process.env.DB_HOST,
-    db_port: parseInt(process.env.DB_PORT, 10),
-    db_name: process.env.DB_NAME,
-    db_user: process.env.DB_USER,
-    db_password: process.env.DB_PASSWORD,
+    db_host: process.env.DB_HOST || 'localhost',
+    db_port: parseInt(process.env.DB_PORT, 10) || '1433',
+    db_name: process.env.DB_NAME || 'testing',
+    db_user: process.env.DB_USER || 'SA',
+    db_password: process.env.DB_PASSWORD || 'Test@123',
     db_logging: process.env.DB_LOGGING === 'true',
 
     // JWT Info
