835a45cb0ae4c661e423f7f464d1ad37bbaaf8de
Justin <jmilliman@serviceexpress.com/>
2023-01-30 13:36:27 -0500

    phone numbers stuff
    Added comments to explain, delete them once you get through them

--- server/models/PhoneNumbers.js
+++ server/models/PhoneNumbers.js
@@ -1,9 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define(
-        'Phone Numbers',
+        'PhoneNumbers', // Leave as non-spaced word
         {
+            // The issue seems to be limited to the constraints. For future reference, here is a link to the docs:
+            // https://sequelize.org/docs/v7/core-concepts/validations-and-constraints/
+            // TODO: Delete me! - There can only be one primary key
+
             phoneNumberId:{
-                type: DataTypes.STRING,
+                type: DataTypes.INTEGER,
                 allowNull: false,
                 field: 'phoneNumberId',
                 primaryKey: true,
@@ -13,27 +17,28 @@ module.exports = (sequelize, DataTypes) => {
                 aliasValue: 'phoneNumberId',
             },
         
+            // TODO: Delete me! - Removing primary key from all other attributes will fix it
+            // TODO: Delete me! - There is another issue. autoIncrement will do a +1 in the SQL table.
+            // TODO: Delete me! - Since this is a reference to driverID/elderID, we don't want that (we want to specify the ID manually)
+            // TODO: Delete me! - So, we can remove the autoIncrement as well. Additionally, this should be an int
             referenceId:{
-                type: DataTypes.STRING,
+                type: DataTypes.INTEGER,
                 allowNull: false,
                 field: 'referenceId',
-                primaryKey: true,
-                autoIncrement: true,
                 searchable: true,
                 alias: 'Reference ID',
                 aliasValue: 'referenceId',
             },
 
-            referenceType:{
+            // TODO: Delete me! - If an attribute is empty, leave it commented out until you know what to do with it. 
+            // referenceType:{
 
-            },
+            // },
 
             phoneNumber:{
                 type: DataTypes.STRING,
                 allowNull: false,
                 field: 'phoneNumber',
-                primaryKey: true,
-                autoIncrement: true,
                 searchable: true,
                 alias: 'Phone Number',
                 aliasValue: 'phoneNumber',
@@ -43,37 +48,31 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.INTEGER,
                 allowNull: false,
                 field: 'phoneExtension',
-                primaryKey: true,
-                autoIncrement: true,
                 searchable: true,
                 alias: 'Phone Extension',
                 aliasValue: 'phoneExtension',
             },
 
             description:{
-                type: DataTypes.ARRAY,
+                type: DataTypes.STRING,
                 allowNull: false,
-                field: 'descrption',
-                primaryKey: false,
-                autoIncrement: true,
+                field: 'description',
                 searchable: true,
                 alias: 'Description',
                 aliasValue: 'description',
             },
 
             order:{
-                type: DataTypes.STRING,
+                type: DataTypes.INTEGER,
                 allowNull: false,
                 field: 'order',
-                primaryKey: false,
-                autoIncrement: true,
                 searchable: true,
                 alias: 'Order',
                 aliasValue: 'order',
             },
         },
         {
-            tableName: "Phone Numbers",
+            tableName: "PhoneNumbers", // leave as non-spaced word
             timeStamps: false,
         }   
         )     
